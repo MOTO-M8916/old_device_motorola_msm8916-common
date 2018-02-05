6944a60782dfe940ba9c3ba2cf3a48a997dca4b4
Mati Halperin <mati@cypheros.co>
2018-02-05 18:27:54 -0300

    msm8916-common: Include legacy QCOM sepolicy rules
    Change-Id: I44de27ff200ff66e74ce9f84560c1419012a1aa4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -124,6 +124,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(VENDOR_PATH)
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/legacy-sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Shims
