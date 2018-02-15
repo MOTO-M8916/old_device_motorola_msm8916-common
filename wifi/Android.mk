b1a0ca31ceefe74781ba092ddef94b39cd6c8ed4
jrior001 <jriordan001@gmail.com>
2018-02-14 22:13:17 -0500

    msm8916-common: move wifi configs to vendor
    Change-Id: Id8b27983fa4ecf4835ae2da8b9031e9d962bdb72

--- wifi/Android.mk
+++ wifi/Android.mk
@@ -9,7 +9,8 @@ LOCAL_MODULE       := p2p_supplicant_overlay.conf
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := $(LOCAL_MODULE)
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
+LOCAL_MODULE_RELATIVE_PATH := wifi
+LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -17,7 +18,8 @@ LOCAL_MODULE       := wpa_supplicant_overlay.conf
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := $(LOCAL_MODULE)
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
+LOCAL_MODULE_RELATIVE_PATH := wifi
+LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
