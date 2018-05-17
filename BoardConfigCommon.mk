aded77f106ac4101ee1d3d3454c43fec6c5555b0
Kyle Harrison <khwebmail@gmail.com>
2018-05-17 18:25:55 +0100

    msm8916-common: DU sepolicy bringup
    Enforcing works in LineageOS with their sepolicy + rules already defined by us in this repo.
    
    So, to bringup sepolicy for DU, import rules from the followng LineageOS directories and customize where necessary.
    
    - device/qcom/sepolicy/legacy-common
    - device/lineage/sepolicy/common
    - device/lineage/sepolicy/qcom
    
    Also add rules required for LD_SHIM_LIBS

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -161,7 +161,6 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(VENDOR_PATH)
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-include device/qcom/sepolicy/legacy-sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Shims
