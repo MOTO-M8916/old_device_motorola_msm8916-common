56d63b031c5879dfd54b2fe60c821fd13e161d25
Erfan Abdi <erfangplus@gmail.com>
2018-06-26 02:41:28 +0430

    msm8916-common: qsap_shim: Fix for VNDK_VERSION=current
    Change-Id: I8e5ec3068914e085d4adac178a47421a1c515a89
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -36,7 +36,7 @@ include $(BUILD_SHARED_LIBRARY)
 # Qsap Shim
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := libqsap_shim.c
-LOCAL_SHARED_LIBRARIES := libqsap_sdk liblog
+LOCAL_SHARED_LIBRARIES := libqsap_sdk liblog libcutils libutils
 LOCAL_C_INCLUDES := $(TOP)/system/qcom/softap/sdk
 LOCAL_MODULE := libqsapshim
 LOCAL_VENDOR_MODULE := true
