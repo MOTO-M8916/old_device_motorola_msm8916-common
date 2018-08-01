5e4232de0f82ac6b34daa942f0219af4f46732c5
Bruno Martins <bgcngm@gmail.com>
2018-08-01 16:28:27 +0200

    msm8916-common:  Build custom QTI telephony extension
    * This extension provides the ability to enable/disable SIM cards individually (the so-called manual SIM provisioning).
    
    * For real this time, using the open source implementation.
    
    Change-Id: Ia2ac619493c5cca6b068c3076e9d293612a219f7

--- msm8916.mk
+++ msm8916.mk
@@ -209,6 +209,14 @@ PRODUCT_PACKAGES += \
     libshim_ril \
     libxml2
 
+# Telephony
+PRODUCT_PACKAGES += \
+    qti-telephony-common \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
