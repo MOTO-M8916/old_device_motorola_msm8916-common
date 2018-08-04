5a42cd211c6b09ffd89c959aa4df8419bc3232c6
Ahmed Hady <ahmedhady6@gmail.com>
2018-08-04 16:39:11 +0200

    fix calling shim libs

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -157,9 +157,9 @@ BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 TARGET_LD_SHIM_LIBS := \
     /system/lib/libandroid.so|/system/vendor/lib/libshim_ril.so \
     /system/vendor/lib/libmdmcutback.so|libqsap_shim.so \
-    /system/vendor/lib/libmot_sensorlistener.so|libsensor.so \
+    /system/vendor/lib/libmot_sensorlistener.so|/system/lib/libsensor.so \
     /system/vendor/lib/libjustshoot.so|libshims_camera.so \
-    /system/vendor/lib/libmmcamera_wavelet_lib.so|libc_util.so \
+    /system/vendor/lib/libmmcamera_wavelet_lib.so|/system/lib/libc_util.so \
     /system/vendor/lib/libqomx_jpegenc.so|libboringssl-compat.so \
     /system/vendor/lib/libmmqjpeg_codec.so|libboringssl-compat.so
 
