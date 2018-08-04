abb402cd131a0121e4c99150fe49866f3010fc75
Ahmed Hady <ahmedhady6@gmail.com>
2018-08-04 17:21:09 +0200

    fix typo

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -159,7 +159,7 @@ TARGET_LD_SHIM_LIBS := \
     /system/vendor/lib/libmdmcutback.so|libqsap_shim.so \
     /system/vendor/lib/libmot_sensorlistener.so|/system/lib/libsensor.so \
     /system/vendor/lib/libjustshoot.so|libshims_camera.so \
-    /system/vendor/lib/libmmcamera_wavelet_lib.so|/system/lib/libc_util.so \
+    /system/vendor/lib/libmmcamera_wavelet_lib.so|libc_util.so \
     /system/vendor/lib/libqomx_jpegenc.so|libboringssl-compat.so \
     /system/vendor/lib/libmmqjpeg_codec.so|libboringssl-compat.so
 
