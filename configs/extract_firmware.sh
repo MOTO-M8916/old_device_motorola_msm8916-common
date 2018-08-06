9e3d51c184251a793cc41cec1640ac6455d2cfda
Ahmed Hady <ahmedhady6@gmail.com>
2018-08-06 12:37:07 +0200

    extract_firmware: drop /system/build.prop form script

--- configs/extract_firmware.sh
+++ configs/extract_firmware.sh
@@ -5,6 +5,5 @@ for file in /firmware/image/*.gz; do
   gzip -dc $file > /vendor/firmware/$OUT_FILE
   chmod 644 /vendor/firmware/$OUT_FILE
   chcon u:object_r:firmware_file:s0 /vendor/firmware/$OUT_FILE
-  chmod 644 "/system/build.prop"
   chmod 644 "/vendor/build.prop"
 done
