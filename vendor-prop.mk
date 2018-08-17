853fa3ca501bda1174dcbdfd25cf3a9d32bf2d7c
althafvly <althafvly@gmail.com>
2018-08-17 19:48:19 +0530

    msm8916-common : Drop hal1

--- vendor-prop.mk
+++ vendor-prop.mk
@@ -32,9 +32,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-	camera2.portability.force_api=1 \
-	media.stagefright.legacyencoder=true \
-	media.stagefright.less-secure=true
+	camera2.portability.force_api=1
 
 # Dalvik Heap
 PRODUCT_PROPERTY_OVERRIDES += \
