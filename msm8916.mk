4e62f2aaaea38aab064c0887c3c7d7e1916f7b7c
Daniel Jarai <jaraidaniel@gmail.com>
2018-09-02 00:34:14 +0200

    Make the external camera provider ignore internal cameras
    * The external camera provider occupies our camera v4l2 nodes,
      potentially crashing the kernel driver and blocking the camera HAL.
      Unfortunately, there's no easy way to just disable it, so let's just
      tell it to ignore the internal video devices.
    
    Change-Id: I00c67de1b94a1f7eca1bf458d1d0e4de3507aa81
    Signed-off-by: Daniel Jarai <jaraidaniel@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -121,6 +121,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	camera2.portability.force_api=1 \
 	debug.camcorder.disablemeta=true
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
+
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images
